6ed6c6d965d54a9749e15ce353ced1d861f83614
Dylan Paris <dylan.paris+github@gmail.com>
2015-08-14 20:54:44 -0700

    Update project.clj description, URL, and license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject phzr "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "A ClojureScript wrapper for the Phaser HTML5 game framework"
+  :url "http://github.com/dparis/phzr"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cljsjs/phaser "2.4.2-0"]
                  [camel-snake-kebab "0.3.2"]])
