45d989ec26636087f6ad46681b32b0d4bfdc5e1d
Dylan Paris <dylan.paris+github@gmail.com>
2015-08-14 21:35:50 -0700

    Configure cider for CLJS repl support in dev

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://github.com/dparis/phzr"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [cljsjs/phaser "2.4.2-0"]
-                 [camel-snake-kebab "0.3.2"]])
+                 [camel-snake-kebab "0.3.2"]]
+
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}}})
