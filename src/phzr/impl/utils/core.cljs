16c1e298ab42a8d9816aad63e25f3cad513c9420
Dylan Paris <dylan.paris+github@gmail.com>
2015-08-15 23:50:54 -0700

    Convert keys in objects returned by phaser->clj

--- src/phzr/impl/utils/core.cljs
+++ src/phzr/impl/utils/core.cljs
@@ -1,4 +1,38 @@
-(ns phzr.impl.utils.core)
+(ns phzr.impl.utils.core
+  (:require [camel-snake-kebab.core :as csk]))
+
+
+(def name->kebab-keyword
+  (memoize
+   (fn [s]
+     (csk/->kebab-case-keyword s))))
+
+(defn ^:private js->clj*
+  [x]
+  (let [keyfn name->kebab-keyword
+        f     (fn thisfn [x]
+                (cond
+                  (satisfies? IEncodeClojure x)
+                  (-js->clj x nil)
+
+                  (seq? x)
+                  (doall (map thisfn x))
+
+                  (coll? x)
+                  (into (empty x) (map thisfn) x)
+
+                  (array? x)
+                  (persistent!
+                   (reduce #(conj! %1 (thisfn %2))
+                           (transient []) x))
+
+                  (identical? (type x) js/Object)
+                  (persistent!
+                   (reduce (fn [r k] (assoc! r (keyfn k) (thisfn (aget x k))))
+                           (transient {}) (js-keys x)))
+
+                  :else x))]
+    (f x)))
 
 (defn clj->phaser
   [x]
@@ -6,4 +40,4 @@
 
 (defn phaser->clj
   [x]
-  (js->clj x))
+  (js->clj* x))
